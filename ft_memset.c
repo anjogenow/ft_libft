d65709ab4290c7c917b5bad7fdd45501ca70e5ae
Anjo Genow <agenow@student.42berlin.de>
2023-11-13 18:04:13 +0100

    Add libft.h, memset, bzero

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agenow <agenow@stdent.42berlin.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 18:02:25 by agenow            #+#    #+#             */
+/*   Updated: 2023/11/13 18:02:32 by agenow           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	i = 0;
+	ptr = (unsigned char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
+	return (b);
+}
