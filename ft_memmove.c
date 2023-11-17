8fa22e4616b53006063aee23396e8fd278ef8155
Anjo Genow <agenow@student.42berlin.de>
2023-11-17 13:46:27 +0100

    Write memmove function

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agenow <agenow@student.42berlin.de>        +#+  +:+       +#+        */
+/*   By: agenow <agenow@stdent.42berlin.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 09:52:45 by agenow            #+#    #+#             */
-/*   Updated: 2023/11/14 09:59:47 by agenow           ###   ########.fr       */
+/*   Updated: 2023/11/17 13:43:57 by agenow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,23 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	size_t	i;
 
 	i = 0;
-	while ()
+	if (!dst && !src)
+		return (dst);
+	if (dst < src)
 	{
-		
+		while (i < len)
+		{
+			((char *) dst)[i] = ((char *) src)[i];
+			i++;
+		}
 	}
+	if (dst > src)
+	{
+		while (len > 0)
+		{
+			((char *) dst)[len] = ((char *) src)[len];
+			len--;
+		}
+	}
+	return (dst);
 }
